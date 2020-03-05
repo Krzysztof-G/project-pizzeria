633bbca11b6d7b717d7925dafa6eee52932db505
Krzysztof Gumuła <k.gumula@icloud.com>
2020-03-05 20:09:45 +0100

    add CartProduct class

--- src/js/script.js
+++ src/js/script.js
@@ -241,6 +241,7 @@
       thisProduct.amount = thisProduct.amountWidget.value;
 
       app.cart.add(thisProduct);
+
     }
   }
 
@@ -345,7 +346,43 @@
 
       thisCart.dom.productList.appendChild(generatedDOM);
 
+      thisCart.products.push(menuProduct);
+
+      console.log('thisCart.products', thisCart.products);
+
       console.log('adding product', menuProduct);
+
+    }
+  }
+
+  class cartProduct{
+    constructor (menuProduct, element){
+
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id;
+      thisCartProduct.name = menuProduct.name;
+      thisCartProduct.price = menuProduct.price;
+      thisCartProduct.priceSingle = menuProduct.priceSingle;
+      thisCartProduct.amount = menuProduct.amount;
+      thisCartProduct.params = JSON.parse(JSON.stringify(menuProduct.params));
+
+      thisCartProduct.getElements(element);
+
+      console.log('new CartProduct', thisCartProduct);
+      console.log('productData', menuProduct);
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {};
+
+      thisCartProduct.dom.wrapper = element;
+      thisCartProduct.dom.amountWidget = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.amountWidget);
+      thisCartProduct.dom.price = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.price);
+      thisCartProduct.dom.edit = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.edit);
+      thisCartProduct.dom.remove = thisCartProduct.dom.wrapper.querySelector(select.cartProduct.remove);
     }
   }
 
